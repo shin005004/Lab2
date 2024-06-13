965dc5535f30c9f7f5276652f3b5478a35feab31
Jeongwoo Shin <39648620+shin005004@users.noreply.github.com>
2024-06-14 00:26:12 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,12 +8,25 @@ vector<int> vec;
 
 void func(int cnt) {
 
-
-
-
-
-
-
+if(cnt == M){
+	    for(auto i : vec) cout << i << ' ';
+	    cout << endl;
+	    return;
+    }
+    if(vec.empty()){
+	    for(int i=1; i<=N; i++){
+		    vec.push_back(i);
+		    func(1);
+		    vec.pop_back();
+	    }
+    }
+    else {
+	for(int i = vec.back()+1; i<=N; i++){
+	   vec.push_back(i);
+	   func(cnt+1);
+	   vec.pop_back();
+    	}
+    }
 }
 
 int main() {
